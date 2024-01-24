48da056211e2b453508ca09524b14ac417099753
Chebannich <chieban.stas@gmail.com>
2024-01-24 01:16:57 +0100

    fix: add deleting of the dir on the begin of the function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,27 +1,30 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const pathToDir = path.join(__dirname, 'files');
 const pathToCopyDir = path.join(__dirname, 'files-copy');
 
-function copyDir(from, to) {
-  fs.mkdir(to, { recursive: true }, (err) => {
+async function copyDir(from, to) {
+  try {
+    await await fs.rm(to, { force: true, recursive: true });
+  } catch (err) {}
+
+  await fs.mkdir(to, { recursive: true }, (err) => {
     if (err) throw err;
   });
-  fs.readdir(from, { withFileTypes: true }, (err, files) => {
-    files.forEach((item) => {
+  const files = await fs.readdir(from, { withFileTypes: true });
+  await Promise.all(
+    files.map(async (item) => {
       const pathToFile = path.join(item.path, item.name);
       const pathToCopyFile = path.join(to, item.name);
 
       if (item.isFile()) {
-        fs.copyFile(pathToFile, pathToCopyFile, (err) => {
-          if (err) throw err;
-        });
+        await fs.copyFile(pathToFile, pathToCopyFile);
       } else {
-        copyDir(pathToFile, pathToCopyFile);
+        await copyDir(pathToFile, pathToCopyFile);
       }
-    });
-  });
+    }),
+  );
 }
 
 copyDir(pathToDir, pathToCopyDir);
